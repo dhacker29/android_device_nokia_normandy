0bc67809df03df4c46dcf2b2aee9bacd3955a28e
dhacker29 <dhackerdvm@gmail.com>
2014-03-14 23:49:50 -0400

    BoardConfig updates

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,23 +1,49 @@
 #
-# config.mk
+# Copyright (C) 2014 The CyanogenMod Project
 #
-# Product-specific compile-time definitions.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# inherit from the proprietary version
+-include vendor/nokia/x/BoardConfigVendor.mk
 
 BOARD_USES_GENERIC_AUDIO := true
 USE_CAMERA_STUB := true
 
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DBINDER_COMPAT
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DNO_UPDATE_PREVIEW
+
 BOARD_HAVE_QCOM_FM := true
 
-TARGET_NO_KERNEL := false
+TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 # Try to use ASHMEM if possible (when non-MDP composition is used)
 TARGET_GRALLOC_USES_ASHMEM := true
 
+# Arch related defines and optimizations
 TARGET_CPU_ABI  := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_BOARD_PLATFORM := msm7627a
+TARGET_BOARD_PLATFORM := msm7x27a
 TARGET_BOOTLOADER_BOARD_NAME := 7x27
 TARGET_CPU_SMP := true
 TARGET_AVOID_DRAW_TEXTURE_EXTENSION := true
@@ -26,6 +52,15 @@ TARGET_USES_16BPPSURFACE_FOR_OPAQUE := true
 TARGET_CORTEX_CACHE_LINE_32 := true
 TARGET_USE_SPARROW_BIONIC_OPTIMIZATION := true
 
+TARGET_USES_ION := true
+TARGET_USES_ASHMEM := true
+TARGET_HAVE_TSLIB := true
+
+# Inline kernel building
+TARGET_KERNEL_SOURCE := kernel/nokia/x
+TARGET_KERNEL_CONFIG := nokiax_defconfig
+
+# Kernel
 BOARD_KERNEL_BASE    := 0x00200000
 BOARD_KERNEL_PAGESIZE := 4096
 #Spare size is (BOARD_KERNEL_PAGESIZE>>9)*16
@@ -56,3 +91,31 @@ BOARD_CACHEIMAGE_PARTITION_SIZE := 41943040
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
 BOARD_HAVE_MXT224_CFG := true
+
+# Audio
+BOARD_USES_SRS_TRUEMEDIA := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+
+# GPS
+TARGET_NO_RPC := false
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50001
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+
+# WLAN
+BOARD_HAS_ATH_WLAN := true
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_HOSTAPD_DRIVER := NL80211
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+HOSTAPD_VERSION := VER_0_8_X
+WIFI_CFG80211_DRIVER_MODULE_PATH := "/system/lib/modules/cfg80211.ko"
+WIFI_CFG80211_DRIVER_MODULE_NAME := "cfg80211"
+WIFI_CFG80211_DRIVER_MODULE_ARG  := ""
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+WIFI_DRIVER_MODULE_ARG := ""
+WIFI_TEST_INTERFACE     := "sta"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_AP  := "ap"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
